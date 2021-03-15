7b6e96947dca71e182b0a3499c94357dd4d978f7
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-15 17:06:10 +0530

    Update model.py

--- model.py
+++ model.py
@@ -19,3 +19,36 @@ class Bert_Classification_Model(nn.Module):
             ids, attention_mask=mask, token_type_ids=token_type_ids)
 
         return self.out(output['pooler_output'])
+
+    
+class RNNOverBERT(nn.Module):
+
+    def __init__(self, bertFineTuned, num_class, hidden_dim=100, yers=1, bidirectional=False):
+        super(RNNOverBERT, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.bidirectional = bidirectional
+        self.bertFineTuned = bertFineTuned
+        
+        self.lstm = nn.LSTM(768, hidden_dim, num_layers=1, bidirectional=bidirectional)
+        self.out = nn.Linear(hidden_dim, num_class)
+
+    def forward(self, ids, mask, token_type_ids, lengt):
+
+        output = self.bertFineTuned(
+            ids, attention_mask=mask, token_type_ids=token_type_ids)
+        chunks_emb = output['pooler_output'].split_with_sizes(lengt)
+
+        seq_lengths = torch.LongTensor([x for x in map(len, chunks_emb)])
+
+        batch_emb_pad = nn.utils.rnn.pad_sequence(
+            chunks_emb, padding_value=-91, batch_first=True)
+        batch_emb = batch_emb_pad.transpose(0, 1)  # (B,L,D) -> (L,B,D)
+        
+        lstm_input = nn.utils.rnn.pack_padded_sequence(
+            batch_emb, seq_lengths.cpu().numpy(), batch_first=False, enforce_sorted=False)
+
+        packed_output, (h_t, h_c) = self.lstm(lstm_input, )  # (h_t, h_c))
+
+        h_t = h_t.view(-1, self.hidden_dim)
+
+        return self.out(h_t)
